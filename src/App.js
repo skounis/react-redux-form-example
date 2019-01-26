09620c99d63e9afdb0f0c4f07a9cb58d227ef716
Stavros Kounis <skounis@gmail.com>
2019-01-26 11:17:27 +0200

    feat(secret) Scaffold secret page. Provide access to authenticated users only.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { LoginPage } from './LoginPage';
+import { SecretPage } from './SecretPage';
 import './App.css';
 
 class App extends Component {
@@ -8,23 +9,47 @@ class App extends Component {
     super(props);
 
     this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
     this.state = {
-      isLoggedIn: false
+      isLoggedIn: false,
+      isSubmitted: false
     };
   }
 
   handleLogin(authenticated) {
-    this.setState({ isLoggedIn: authenticated });
-    console.log('Handle Login. Set isLoggedIn to: ', authenticated)
+    this.setState({ 
+      isLoggedIn: authenticated,
+      isSubmitted: true 
+    });
+  }
+
+  handleLogout(authenticated) {
+    this.setState({ 
+      isLoggedIn: false,
+      isSubmitted: false 
+    });
   }
 
   render() {
     return (
       <div className="App jumbotron">
         <div className="container">
-          <LoginPage 
-            isLoggedIn={this.state.isLoggedIn} 
-            onLoginSubmit={this.handleLogin} />
+          {!this.state.isLoggedIn && this.state.isSubmitted &&
+            <div className="row justify-content-center">
+              <div className="col-md-6">
+                <div className="alert alert-danger">Username or password is incorrect</div>
+              </div>
+            </div>
+          }
+          {!this.state.isLoggedIn &&
+            <LoginPage 
+              isLoggedIn={this.state.isLoggedIn} 
+              onLoginSubmit={this.handleLogin} />
+          }
+          {this.state.isLoggedIn &&
+            <SecretPage 
+            onLogout={this.handleLogout}/>
+          }
         </div>
       </div>
     );
---
+++ src/SecretPage/SecretPage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class SecretPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleLogout(e) {
+    e.preventDefault();
+    console.log('Loging out ...');
+    this.props.onLogout();
+  }
+
+  render() {
+    return (
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <h2>Welcome</h2>
+          <p>You have unlocked a secret page.</p>
+          <div className="form-group">
+            <button 
+              onClick={this.handleLogout}
+              className="btn btn-primary">Logout</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export { SecretPage }; 
