58227a3c2dd0a42561ce49ce5f7bb41f1427f103
Stavros Kounis <skounis@gmail.com>
2019-01-27 11:11:00 +0200

    feat(secret) Welcome the logged in user.

--- src/SecretPage/SecretPage.js
+++ src/SecretPage/SecretPage.js
@@ -18,7 +18,7 @@ class SecretPage extends React.Component {
       <div className="row justify-content-center">
         <div className="col-md-6">
           <h2>Welcome</h2>
-          <p>You have unlocked a secret page.</p>
+          <p>Welconme <code>{this.props.user.username}</code>. You have unlocked a secret page.</p>
           <div className="form-group">
             <button 
               onClick={this.handleLogout}
